9e9d846e745358c52ac63d76e5308598be282f1c
Sergey Kurguzov <korbendetka264@mail.ru>
2023-09-10 17:49:47 +0400

    Adding logic with application parameters

---
+++ src/components/applicationParamsSlice.js
@@ -0,0 +1,47 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    applicationParams: {
+        sliderPosition: {
+            start: 1,
+            end: 8,
+        },
+        activeArrowButton: {
+            left: false,
+            right: true,
+        }
+    },
+}
+
+const applicationParamsSlice = createSlice({
+    name: '@@app-params',
+    initialState,
+    reducers: {
+        addSliderPosition: (state, action) => {
+            let { start, end } = action.payload
+            state.applicationParams.sliderPosition = {
+                start,
+                end,
+            }
+        },
+        addActiveArrowButton: (state, action) => {
+            let { left, right } = action.payload
+            state.applicationParams.activeArrowButton = {
+                left,
+                right,
+            }
+        },
+        // addParams: (state, action) => {
+        //     for (let key in action.payload) {
+        //         state.applicationParams[key] = action.payload[key]
+        //     }
+        // },
+        removeParams: (state) => {
+            state.applicationParams = initialState.applicationParams
+        },
+    }
+})
+
+
+export const { addActiveArrowButton, addSliderPosition, removeParams } = applicationParamsSlice.actions
+export const applicationParamsReducer = applicationParamsSlice.reducer
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { regionsReducer, weatherForecastReducer } from '../components/weatherForecastComponent/weatherForecastSlice'
+import { applicationParamsReducer } from '../components/applicationParamsSlice'
 
 export const store = configureStore({
     reducer: {
         regionsState: regionsReducer,
         weatherForecastState: weatherForecastReducer,
+        userParams: applicationParamsReducer,
     },
     devTools: true,
 })
